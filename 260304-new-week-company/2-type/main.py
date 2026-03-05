6e8b67b70a872edafcb5caa04662073fba60a64b
cheoljoo.lee <cheoljoo.lee@lge.com>
2026-03-05 12:37:09 +0900

    -

--- 260304-new-week-company/2-type/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from 2-type!")
-
-
-if __name__ == "__main__":
-    main()
--- 260304-new-week-company/4-type/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from 4-type!")
-
-
-if __name__ == "__main__":
-    main()
